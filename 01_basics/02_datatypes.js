a8913cd382902245da6bf835a56f2bc4403c384b
Hanash-Ahad <hanashahad22@gmail.com>
2025-04-23 10:57:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all CODE with a newer version of JS
+
+// alert(3 + 3); // this won't run because we r using node.js, not browser
+
+console.log(3 
+    + 
+    3) //code readability must be high
+
+console.log("Hanash");
+
+
+let name = "Hanash"; // string
+let age = 25; // int
+let isLoggedIn = true; // bollean
+let state; //undefined
+// let state = null // this is a null value
+
+//num => 2 to the power 53
+//bigint
+//string =>""
+//boolean => true or false
+//null => stand alone value
+// undefined
+//symbol => used for uniqueness
+
+//object
+
+console.log(typeof undefined); // undefined datatype
+console.log(typeof null); // null is an object by itself
