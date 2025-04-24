aee0f015e70414342b96f7b46243aa52922c1a1d
Hanash-Ahad <hanashahad22@gmail.com>
2025-04-24 12:33:53 +0000

    nums and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,30 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(333);
+// console.log(balance);
+
+// console.log(balance.toString()); // converts to string
+// console.log(balance.toFixed(3)); // to assign precision value
+// console.log(balance.toFixed(2));
+// console.log(balance.toFixed(1));
+
+
+const otherNum = 23.4636;
+// console.log(otherNum.toPrecision(4)); // gives precision according to given numbers
+// console.log(otherNum.toPrecision(3)); // gives precision according to given numbers
+// console.log(otherNum.toPrecision(2)); // gives precision according to given numbers
+
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString()); // separates numbers accordingly
+
+
+
+// +++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++ \\
+
+console.log(Math);
+console.log(Math.abs(-4)); // it converts negative numbers into positive
+console.log(Math.round(4.6)); // gives round about value
+
+
