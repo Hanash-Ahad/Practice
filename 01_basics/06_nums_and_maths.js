b4131715a3e2436d3181c9e60102529dbb7bdae8
Hanash-Ahad <hanashahad22@gmail.com>
2025-04-27 19:53:36 +0000

    nums and maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -23,8 +23,21 @@ const hundreds = 1000000;
 
 // +++++++++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++++++++++++++ \\
 
-console.log(Math);
-console.log(Math.abs(-4)); // it converts negative numbers into positive
-console.log(Math.round(4.6)); // gives round about value
+// console.log(Math);
+// console.log(Math.abs(-4)); // it converts negative numbers into positive
+// console.log(Math.round(4.6)); // gives round about value
+// console.log(Math.ceil(4.1)); // it choose the top value
+// console.log(Math.floor(4.1)); // it choose the lower value
+// console.log(Math.min(4, 3, 5, 1, 8)); // it finds the lowest value
+// console.log(Math.max(4, 3, 5, 1, 8)); // it finds the highest value
 
+
+console.log(Math.random()); // it gives the value between 0 and 1.
+console.log((Math.random()*10) + 1); // Gives a random integer from 1 to 10
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max - min + 1)) + min); // Will randomly log a number between 10 and 20.
 
